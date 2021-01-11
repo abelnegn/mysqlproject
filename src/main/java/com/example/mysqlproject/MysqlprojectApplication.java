9ec92716693579489d5e6cebf3e968858e66d862
abel tadesse <abtadesse@kifiya.com>
2021-01-11 11:21:13 +0300

    commited

--- src/main/java/com/example/mysqlproject/MysqlprojectApplication.java
+++ src/main/java/com/example/mysqlproject/MysqlprojectApplication.java
@@ -12,7 +12,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.List;
 
 @SpringBootApplication
-public class MysqlprojectApplication {
+public class MysqlprojectApplication implements CommandLineRunner{
 
 	@Autowired
 	private ProfileService profileService;
@@ -21,26 +21,72 @@ public class MysqlprojectApplication {
 		SpringApplication.run(MysqlprojectApplication.class, args);
 	}
 
-	/*@Override
+	@Override
 	public void run(String... strings) throws Exception {
 
-		Profile newProfile = new Profile();
-		newProfile.setName("Dembitu");
-		newProfile.setFname("Hambisa");
-		newProfile.setAge(18);
+/*		int arr[] = {10,20,30,40,50, 60, 70, 80, 90};
+		int key = 90;
+		int last=arr.length-1;
+		binarySearch(arr,0,last,key);*/
 
-		int result = profileService.saveProfile(newProfile);
+		int arr[] ={3,60,35,2,45,320,5};
 
-		if(result > 0){
-			System.out.println("A new row inserted");
+		System.out.println("Array Before Bubble Sort");
+		for(int i=0; i < arr.length; i++){
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+
+		bubleSort(arr);//sorting array elements using bubble sort
+
+		System.out.println("Array After Bubble Sort");
+		for(int i=0; i < arr.length; i++){
+			System.out.print(arr[i] + " ");
 		}
 
+	}
 
-		String selectSql = "SELECT * FROM profile";
-		List<Profile> profiles = profileService.profileList(selectSql);
+	private void binarySearch(int array[], int first, int last, int key){
+		int mid = (first + last)/2;
 
-		if(profiles != null){
-			System.out.println("List of Customer profiles are");
+		while(first <= last){
+			if(array[mid] < key){
+				first = mid + 1;
+			}else if(array[mid] == key){
+				System.out.println("Element is found at index " + mid);
+				break;
+			}else{
+				last = mid - 1;
+			}
+			mid = (first + last)/2;
 		}
-	}*/
+		if(first > last){
+			System.out.println("Element is not found");
+		}
+	}
+
+	private int linearSearch(int[] arr, int key){
+		for(int i=0;i<arr.length;i++){
+			if(arr[i] == key){
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	private void bubleSort(int[] arr){
+		int n = arr.length;
+		int temp = 0;
+
+		for(int i=0; i<n; i++){
+			for(int j=1; j < (n-i); j++){
+				if(arr[j-1] > arr[j]){
+					//swap elements
+					temp = arr[j-1];
+					arr[j-1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	}
 }
